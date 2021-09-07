88efe0eb5124d2980457cfc0fad73500d54bd00c
aamir <2019aamir.ansari@ves.ac.in>
2021-09-07 11:00:35 +0530

    :hammer: rework

--- LabWork - Assignments/DSA/Experiment 01/Code/Implementation of stack.c
+++ LabWork - Assignments/DSA/Experiment 01/Code/Implementation of stack.c
@@ -31,69 +31,65 @@ int main()
         printf("\n* 6 -> EXIT\n");
         scanf("%d", &num);
         printf("\n\t");
-        if (num == 1)
-        { //PUSH
+        if (num == 1) { 
             printf("\n--Enter a number to push--   ");
             scanf("%d", &elem);
             push(elem);
         }
-        else if (num == 2)
-        { //POP
+        else if (num == 2) { 
             pop(&popped);
         }
-        else if (num == 3)
-        { //PEEK
+        else if (num == 3) { 
             peek(&peeked);
         }
-        else if (num == 4)
-        { //DISPLAY
+        else if (num == 4) { 
             display();
         }
-        else if (num == 5)
-        { //SIZE
+        else if (num == 5) { 
             printf("\n--Currently, size of stack is  =>  %d", size());
         }
-        else if (num == 6)
-        { //QUIT
+        else if (num == 6) { 
             printf("\n\n************************\n");
             break;
         }
-        else
-        { //DEFAULT
+        else { 
             printf("\nINVALID INPUT");
         }
     }
     return 0;
 }
 
-void initialize()
-{
+// ? To initialize a new stack
+void initialize() {
     top = -1;
 }
 
-int isEmpty()
-{
+// ? check if stack is empty
+    // 1 if empty
+    // 0 if not empty
+int isEmpty() {
     if (top == -1)
         return 1;
     return 0;
 }
 
-int size()
-{
+// ? Return size of stack
+int size() {
     return top + 1;
 }
 
-int isFull()
-{
+// ? check if stack is full
+    // 1 if stack is full
+    // 0 if stack is not full
+int isFull() {
     if (top == MAX - 1)
         return 1;
     return 0;
 }
 
-void push(int elem)
-{
-    if (isFull())
-    {
+// ? insert elemtent in stack
+void push(int elem) {
+    if (isFull()) {
         printf("\nOVERFLOW");
         return;
     }
@@ -101,10 +97,9 @@ void push(int elem)
     stack[top] = elem;
 }
 
-void pop(int *popped)
-{
-    if (isEmpty())
-    {
+// ? remove element from stack
+void pop(int *popped) {
+    if (isEmpty()) {
         printf("\nUNDERFLOW");
         return;
     }
@@ -113,8 +108,8 @@ void pop(int *popped)
     printf("--Popped element is  =>  %d", *popped);
 }
 
-void peek(int *peeked)
-{
+// ? prints the top element of stack
+void peek(int *peeked) {
     if (isEmpty())
     {
         printf("\nUNDERFLOW");
@@ -124,18 +119,15 @@ void peek(int *peeked)
     printf("--Top value is  =>  %d", *peeked);
 }
 
-void display()
-{
+// ? display all the contents of stack
+void display() {
     int i;
-    if (isEmpty())
-    {
+    if (isEmpty()) {
         printf("\nEMPTY");
     }
-    else
-    {
+    else {
         printf("\nElements in Stack are :  ");
-        for (i = 0; i <= top; i++)
-        {
+        for (i = 0; i <= top; i++) {
             printf("%d\t", stack[i]);
         }
     }
